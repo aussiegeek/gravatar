7c4b18428de7f10dd91a4d24a5a9c6f8ffe043bb
Alan Harper <alan@aussiegeek.net>
2009-05-16 21:47:07 +1000

    Fix new style gravatar URL fail

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :avatars
-
-  map.resources :avatars
-
+  map.avatar '/avatar/:id.:format', :controller => 'avatars', :action => 'show'
   map.php '/avatar.php', :controller => 'avatars', :action => 'show', :format => 'png'
   map.root :controller => 'avatars'
 end
